dfa11a930d98c583d795e46eee3f749c501abde7
sergejgrigorev2861-sys <sergejgrigorev2861@gmail.com>
2026-04-10 18:39:40 +0300

    Исправления: добавлены docstring, type hints, isort, пустая строка

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@
 а так же для форматирования дат из ISO-формата
 """
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
